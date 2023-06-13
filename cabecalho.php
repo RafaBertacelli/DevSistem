458336b3d3527565c147f53272c9c5903402a317
Rafa Bertacelli <rafabertacelli2303@gmail.com>
2023-06-12 23:39:41 -0300

    thre commit

---
+++ cabecalho.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <title>Dev Sistem</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <div class="cabecalho-imagem">
+            <a href="index.php"><img src="img/logo.png" alt="logo" class="cabecalho-imagem-logo"></a>
+        </div>
+        <div class="icon">
+            <button>
+                <i class="fa-regular fa-circle-user" ></i>
+            </button>
+            
+        </div>
+        
+        <div class="input-group">
+            <input type="text" class="texto" placeholder="Pesquisar">
+            <button>
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+        <img src="img/navBar.png" alt="navNar" class="cabecalho-imagem-navBar">
+  </header>
+
+  <nav class="cabecalho-menu">
+        <a class="cabecalho-menu-item" href="noticias.php">NOTÍCIAS</a>
+        <a class="cabecalho-menu-item" href="#">TUTORIAS</a>
+        <a class="cabecalho-menu-item" href="#">SOBRE</a>
+        <a class="cabecalho-menu-item"
+        href="contato.php">FALE CONOSCO</a>
+    </nav>
+
+    
+
+    
+
+
+    
+   
+
+    
---
+++ footer.php
@@ -0,0 +1,74 @@
+<footer>
+            <div id="footer_content">
+                <div id="footer_contacts">
+                    <h1>Logo</h1>
+                    <p>Falar sobre</p>
+
+                    <div id="footer_social_midia">
+                        <a href="#" class="footer link" id="instagram">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+
+                        <a href="#" class="footer link" id="facebook">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a>
+
+                        <a href="#" class="footer link" id="whatsapp">
+                            <i class="fa-brands fa-whatsapp"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <ul class="footer-list">
+                    <li>
+                        <h1>Blog</h1>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-list">Ola</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-list">Tudo</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-list">Bem</a>
+                    </li>
+                </ul>
+
+                <ul class="footer-list">
+                    <li>
+                        <h1>Producao</h1>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-list">Oi</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-list">Sim</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-list">Muito</a>
+                    </li>
+                </ul>
+
+                <div id="footer_subscribe">
+                    <p>
+                        Amem senhor
+                    </p>
+
+                    <div id="input_group">
+                        <input type="email" id="email">
+                        <button>
+                            <i class="fa-regular fa-envelope"></i>
+                        </button>
+
+                    </div>
+                </div>
+
+                </div>
+                <div id="footer_copyrigth">
+                    &#169
+                    2023 all alguma coisa
+                </div>
+
+        </footer>
+</body>
+</html>
