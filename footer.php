3027543e0a87327a80b1dff58335ed29c0c7919d
Rafa Bertacelli <rafabertacelli2303@gmail.com>
2023-06-13 00:44:34 -0300

    alterei o footer

--- footer.php
+++ footer.php
@@ -1,74 +1,56 @@
 <footer>
-            <div id="footer_content">
-                <div id="footer_contacts">
-                    <h1>Logo</h1>
-                    <p>Falar sobre</p>
-
-                    <div id="footer_social_midia">
-                        <a href="#" class="footer link" id="instagram">
-                            <i class="fa-brands fa-instagram"></i>
+            <div class="footer">
+                <div class="footer-midia">
+                    <div class="footer-titulo">DevSistem</div>
+                    <p>Falem conosco!</p>
+    
+                    <div class="footer-midia-social">
+                        <a href="#" class="footer-instagram" id="instagram">
+                            <i class="footer-instagram-img"><img src="img/instagram.png"></i>
                         </a>
 
-                        <a href="#" class="footer link" id="facebook">
-                            <i class="fa-brands fa-facebook-f"></i>
+                        <a href="#" class="footer-wpp" id="wpp">
+                            <i class="footer-wpp-img"><img src="img/whatsapp.png"></i>
                         </a>
 
-                        <a href="#" class="footer link" id="whatsapp">
-                            <i class="fa-brands fa-whatsapp"></i>
+                        <a href="#" class="footer-github" id="github">
+                            <i class="footer-github-img"><img src="img/github.png"></i>
                         </a>
                     </div>
                 </div>
-
-                <ul class="footer-list">
-                    <li>
-                        <h1>Blog</h1>
-                    </li>
-                    <li>
-                        <a href="#" class="footer-list">Ola</a>
-                    </li>
-                    <li>
-                        <a href="#" class="footer-list">Tudo</a>
-                    </li>
-                    <li>
-                        <a href="#" class="footer-list">Bem</a>
-                    </li>
-                </ul>
-
-                <ul class="footer-list">
-                    <li>
-                        <h1>Producao</h1>
-                    </li>
-                    <li>
-                        <a href="#" class="footer-list">Oi</a>
-                    </li>
-                    <li>
-                        <a href="#" class="footer-list">Sim</a>
-                    </li>
-                    <li>
-                        <a href="#" class="footer-list">Muito</a>
-                    </li>
-                </ul>
-
-                <div id="footer_subscribe">
-                    <p>
-                        Amem senhor
-                    </p>
-
-                    <div id="input_group">
-                        <input type="email" id="email">
-                        <button>
-                            <i class="fa-regular fa-envelope"></i>
-                        </button>
-
-                    </div>
+                <div class="footer-item">
+                    <ul class="footer-item1">
+                        <li>
+                            <h1>Blog</h1>
+                        </li>
+                        <li>
+                            <a href="#" class="footer-letrinhas">Ola</a>
+                        </li>
+                        <li>
+                            <a href="#" class="footer-letrinhas">Tudo</a>
+                        </li>
+                        <li>
+                            <a href="#" class="footer-letrinhas">Bem</a>
+                        </li>
+                    </ul>
                 </div>
-
+                <div class="footer-item">
+                    <ul class="footer-item2">
+                        <li>
+                            <h1>Blog</h1>
+                        </li>
+                        <li>
+                            <a href="#" class="footer-letrinhas">Ola</a>
+                        </li>
+                        <li>
+                            <a href="#" class="footer-letrinhas">Tudo</a>
+                        </li>
+                        <li>
+                            <a href="#" class="footer-letrinhas">Bem</a>
+                        </li>
+                    </ul>
                 </div>
-                <div id="footer_copyrigth">
-                    &#169
-                    2023 all alguma coisa
-                </div>
-
+            </div>
         </footer>
 </body>
 </html>
