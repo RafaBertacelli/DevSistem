50cbfa67da7e0ca6caa3272f8afc49299282a6b9
Rafinha <131063922+RafaBertacelli@users.noreply.github.com>
2023-06-18 20:54:10 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -10,25 +10,33 @@
     <title>Dev Sistem</title>
 </head>
 <body>
-    <header class="cabecalho">
+   <header class="cabecalho">
+      <div class="logo">  
         <div class="cabecalho-imagem">
             <a href="index.php"><img src="img/logo.png" alt="logo" class="cabecalho-imagem-logo"></a>
         </div>
+     </div>
+     <div class="itens-cabecalho">
         <div class="icon">
             <button>
                 <i class="fa-regular fa-circle-user" ></i>
             </button>
-            
+           
         </div>
-        
-        <div class="input-group">
-            <input type="text" class="texto" placeholder="Pesquisar">
-            <button>
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
+        <div class="input">
+            <div class="input-group">
+                <input type="text" class="texto" placeholder="Pesquisar">
+                <button>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
         </div>
-        <img src="img/navBar.png" alt="navNar" class="cabecalho-imagem-navBar">
-  </header>
+        <div class="nav-bar">
+            <img src="img/navBar.png" alt="navNar" class="cabecalho-imagem-navBar">
+        </div>
+    </div>
+    </header>
+
 
   <nav class="cabecalho-menu">
         <a class="cabecalho-menu-item" href="noticias.php">NOTÍCIAS</a>
