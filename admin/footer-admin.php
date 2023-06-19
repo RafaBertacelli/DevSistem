cba388fe77f00e2ac44a271c583e3a6dc19d0a0d
Nicoly Silva <112127566+niczonha@users.noreply.github.com>
2023-06-19 10:56:47 -0300

    Create footer-admin.php

---
+++ admin/footer-admin.php
@@ -0,0 +1,48 @@
+<footer>
+            <div class="footer-container">
+                <div class="footer-coluna1">
+
+                    <h1>Dev Sistem</h1>
+                    <br>
+                    <p>Empresa de tecnologia para impulsionar negócios digitais.  Somos especialistas em desenvolvimento de negócios digitais, trabalhando em projetos de Tecnologia desde 2022.
+                    </p>
+                    <br>
+                    <div class="icons">
+                        
+                        <div class="instagram">
+                                <i class="fa-brands fa-instagram"></i>
+                        </div>
+
+                        <div class="facebook">
+                                <i class="fa-brands fa-facebook"></i>
+                        </div>
+
+                        <div class="github">
+                                <i class="fa-brands fa-github"></i>
+                        </div>
+
+                    </div>
+                </div>    
+                
+                <div class="footer-coluna2">
+
+                    <h1>São Paulo, Brasil</h1>
+                    <br>
+                    <p>R. Feliciano de Mendonça, 290 - Guaianases, São Paulo</p>
+                    <br>
+                    <p>SP, 08460-365</p>
+                    <br>
+                    <p>contato@devSistem.com.br</p>
+
+                </div>
+            </div>
+
+            <div class="copyright">
+
+                    <p>&#169 2023 all right reserver</p>
+                    
+                </div>
+                                
+        </footer>
+</body>
+</html>
