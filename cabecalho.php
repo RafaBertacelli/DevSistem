08ae62a5d7a6879d9d5971ebf5658c9004318011
Nicoly Silva <112127566+niczonha@users.noreply.github.com>
2023-06-19 23:47:57 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <title>Dev Sistem</title>
+</head>
 <body>
    <header class="cabecalho">
       <div class="logo">  
@@ -36,12 +47,3 @@
     </nav>
 
 
-    
-
-    
-
-
-    
-   
-
-    
