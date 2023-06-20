79550658311d5acbb0a22e7b698b86d0ce20391a
isabella <119014328+isah17@users.noreply.github.com>
2023-06-19 22:31:08 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -2,38 +2,9 @@
 include("cabecalho.php");
 ?>
 <?php include("conexao.php"); ?>
-<?php
-    //$stmt = $pdo->prepare("select * from tbcontato");	
-   // $stmt ->execute();
-    
-   // while($row = $stmt->fetch(PDO::FETCH_BOTH)){       
-    //    echo $row["idContato"];						
-    //    echo $row[1];						
-     //   echo $row[2];						
-      //  echo $row[3];											
-      //  echo $row[4];
-      //  echo "<br />";      
-    //}	
-    if(isset($_POST['submit']))
-    {
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $assunto = $_POST['assunto'];
-        $mensagem = $_POST['mensagem'];
-	
-       // $result = PDO_query($pdo, "INSERT INTO tbContato(nome,email,assunto,mesagem)VALUES('$nome','$email','$assunto','$mensagem')");
-        echo "<div class='infor'>";
-        print_r('Nome: '. $_POST['nome']);
-        print_r('<br>');
-        print_r('Email: '.$_POST['email']);
-        print_r('<br>');
-        print_r('Assunto: '.$_POST['assunto']);
-        print_r('<br>');
-        print_r('Mensagem: '.$_POST['mensagem']);
-        echo "</div>";
-    }
-?>
-<main>
+
+
+ <main>
     <selection>
         <div class="contato-container">
             <div class="contato-mensagem">
@@ -52,17 +23,17 @@ include("cabecalho.php");
                     Envie sua mensagem
                 </div>
 
-                <form class="form-contato" action="contato.php" method="POST">
+                <form class="form-contato" action="noticias.php" method="POST">
                     <div class="contato-input-div">
-                        <input class="contato-input" type="text" placeholder="Digite o seu nome completo*" name="txNome" required>
+                        <input class="contato-input" type="text" placeholder="Digite o seu nome completo*" name="nome" required>
                     </div>
 
                     <div class="contato-input-div">
-                        <input class="contato-input" type='email' id='email' name='txEmail' placeholder="Digite seu Email*" required>
+                        <input class="contato-input" type='email' id='email' name='email' placeholder="Digite seu Email*" required>
                     </div>
 
                     <div class="contato-input-div">
-                        <input class="contato-input" type="text" placeholder="Digite o assunto" name="txAssunto" required>
+                        <input class="contato-input" type="text" placeholder="Digite o assunto" name="assunto" required>
                     </div>
 
                     <div class="contato-mensagem-textinho-div">
@@ -70,11 +41,11 @@ include("cabecalho.php");
                     </div>
 
                     <div class="contato-input-mensagem-div">
-                        <input class="contato-input-mensagem" type="text"  name="txMensagem" required>
+                        <input class="contato-input-mensagem" type="text"  name="mensagem" required>
                     </div>
                         
                     <div class="contato-bt">
-                        <input class="contato-botao" type="submit" value="Enviar">                       
+                        <input class="contato-botao" type="submit" name="submit" id="submit" value="Enviar">                       
                     </div>
 
                 </form>
@@ -88,4 +59,3 @@ include("cabecalho.php");
 <?php 
 include("footer.php");
 ?>
-
