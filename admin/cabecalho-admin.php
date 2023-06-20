cfaa3bd194e58506af03bf683fc2afd6cff5aeeb
Nicoly Silva <112127566+niczonha@users.noreply.github.com>
2023-06-19 23:48:24 -0300

    Update cabecalho-admin.php

--- admin/cabecalho-admin.php
+++ admin/cabecalho-admin.php
@@ -13,7 +13,7 @@
    <header class="cabecalho">
       <div class="logo">  
         <div class="cabecalho-imagem">
-            <a href="index.php"><img src="../img/logo.png" alt="logo" class="cabecalho-imagem-logo"></a>
+            <a href="../index.php"><img src="../img/logo.png" alt="logo" class="cabecalho-imagem-logo"></a>
         </div>
      </div>
      <div class="itens-cabecalho">
@@ -23,29 +23,10 @@
             </button>
            
         </div>
-        <div class="input">
-            <div class="input-group">
-                <input type="text" class="texto" placeholder="Pesquisar">
-                <button>
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-            </div>
-        </div>
-        <div class="nav-bar">
-            <img src="../img/navBar.png" alt="navNar" class="cabecalho-imagem-navBar">
-        </div>
+       
+       
     </div>
     </header>
-
-
-  <nav class="cabecalho-menu">
-        <a class="cabecalho-menu-item" href="noticias.php">NOTÍCIAS</a>
-        <a class="cabecalho-menu-item" href="#">TUTORIAS</a>
-        <a class="cabecalho-menu-item" href="#">SOBRE</a>
-        <a class="cabecalho-menu-item"
-        href="contato.php">FALE CONOSCO</a>
-    </nav>
-
     
 
     
