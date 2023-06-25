ede463d3c230eb46963b9af4190e5f8f9b3c7f30
isabella <119014328+isah17@users.noreply.github.com>
2023-06-25 20:10:20 -0300

    Update noticias.php

--- noticias.php
+++ noticias.php
@@ -1,1 +1,199 @@
+<?php
+include("cabecalho.php");
+?>
+
+<main class="conteudo">
+    <section>
+        <div class="titulo-container">Destaques</div>
+        
+        <div class="container">
+            
+            <div class="c3">
+                <img class="caixa3" src="img/dinheiro.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Saiba tudo
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Como ganhar muito dinheiro programando
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/Design.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Você sabe?
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Qual a função de um Design Gráfico?
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/Design.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Você sabe?
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Qual a função de um Design Gráfico?
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/Design.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Você sabe?
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Qual a função de um Design Gráfico?
+                    </div>
+                </div>              
+            </div>
+
+        </div>
+        <div class="titulo-container">Linguagens mais usadas</div>
+        
+        <div class="container">
+            <div class="c3">
+                <img class="caixa3" src="img/Faculdade.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Se inscreva!
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Faculdade 100% gratuita na área de programação
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/aprendiz.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Descubra!
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Cursos de desenvolvedor para jovens de até 18 anos
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/3.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Saiba mais!
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Google abre vagas para cursos de programação online e com 70% de desconto
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/ProgramandoPython.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Entenda
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Governo libera curso de python gratuito nas escolas públicas
+                    </div>
+                </div>              
+            </div>
+           
+            
+        </div>
+        <div class="titulo-container">Algumas dicas</div>
+        
+        <div class="container">
+            <div class="c3">
+                <img class="caixa3" src="img/Faculdade.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Se inscreva!
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Faculdade 100% gratuita na área de programação
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/aprendiz.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Descubra!
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Cursos de desenvolvedor para jovens de até 18 anos
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/3.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Saiba mais!
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Google abre vagas para cursos de programação online e com 70% de desconto
+                    </div>
+                </div>              
+            </div>
+            <div class="c3">
+                <img class="caixa3" src="img/ProgramandoPython.png" alt="">
+                <div class="textos-caixas">
+                    <div class="title-c">
+                        <a href="#">
+                            Entenda
+                        </a>
+                    </div>
+                    
+                    <div class="text-c">
+                        Governo libera curso de python gratuito nas escolas públicas
+                    </div>
+                </div>              
+            </div>
+           
+            
+        </div>
+ 
+    </section>
+</main>
+                <?php 
+include("footer.php");
+?>
 
