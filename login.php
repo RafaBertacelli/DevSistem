28d8df09ef988b54ad9b557cd777b01db1f16888
Nicoly Silva <112127566+niczonha@users.noreply.github.com>
2023-06-26 11:41:53 -0300

    Update login.php

--- login.php
+++ login.php
@@ -2,46 +2,53 @@
 include("cabecalho.php");
 ?>
 
-<div class="textoLogin">
-    <h2>Faça seu login</h2>
-</div>
+
 
 
 <div class="login">
-    <div class="tela1">
-        <div class="textoLogin">
-            <h2>Já sou cadastrado</h2>   
-        </div>
+            <div class="contato-mensagem">
+                <div class="contato-mensagem-titulo">
+                    Faça o login! <br> Bem-vindo de volta. 
+                </div>
+                <br>
+                <div class="contato-mensagem-textinho">
+                   Novo por aqui? Cadastre-se já!
+                </div>
+            </div>
 
-        <br>
             <div class="inputs">
-                <form action="">
+                <div class="textoLogin">
+                    <h2>Já sou cadastrado</h2>   
+                </div>
+
+                <br>
+
+                <form class="form-login" action="">
+
                         <div class="usuario">
                             <input type="text" name="txUser" placeholder="Digite seu email" class="caixa-login">
                         </div>
+
                         <br>
 
                         <div class="senha">
                             <input type="text" name="txSenha" placeholder="Digite sua senha" class="caixa-login">
                         </div>
-                    <br>
+
+                        <br>
 
                         <div class="enviarLogin">
                             <input type="submit" name="txEnviar" value="Entrar" class="botao-login">
                         </div>
-                    </div>
-                </form>
-               
-    </div>
-    <div class="tela2">
-        <div class="textoLogin">
-            <h2>Quero me cadastrar</h2>
-        </div>
-        <br>
-        <div class="enviarLogin">
-            <input type="submit" name="txEnviar" value="Cadastrar com email" class="botao-login-cadastro">
-        </div>
-    </div>
 
-       
+                        <div class="login-nao-cadastrado">
+                            <a href="#">Não tem uma conta? Cadastre-se!</a>
+                        </div>
+                </form>
+
+
+            </div>        
 </div>
+<?php 
+include("footer.php");
+?>
